8b8fea549544f9acbe97790069e8e7b7904a90aa
Ariful2634 <ariful2634@gmail.com>
2024-02-02 12:26:37 +0600

    setup the right side drawer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,32 @@
 "use client"
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import img from '../assets/profile 1.png'
 import Image from 'next/image';
 import { IoSearchOutline } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    useEffect(() => {
+        const closeDrawerOnOutsideClick = (event) => {
+          if (open && !event.target.closest('.drawer')) {
+            setOpen(false);
+          }
+        };
+    
+        document.addEventListener('click', closeDrawerOnOutsideClick);
+    
+        return () => {
+          document.removeEventListener('click', closeDrawerOnOutsideClick);
+        };
+      }, [open]);
+
+    // console.log(open)
     return (
         <div className='w-[92%]'>
             <div className="navbar ">
@@ -14,11 +36,11 @@ const Navbar = () => {
                 <div className="flex-none gap-40">
                     <div className="form-control hidden lg:block">
                         <input type="text" placeholder="Search by Dua Name" className="input input-bordered w-24 md:w-auto relative" />
-                        <div className='w-[35px] text-center rounded absolute p-2 top-4 right-[230px] bg-gray-300'>
-                        <IoSearchOutline></IoSearchOutline>
+                        <div className='w-[35px] text-center rounded absolute p-2 top-4 right-[250px] bg-gray-300'>
+                            <IoSearchOutline></IoSearchOutline>
                         </div>
                     </div>
-                    <div className="dropdown dropdown-end">
+                    <div className="dropdown dropdown-end flex items-center">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar hidden lg:block">
                             <div className="w-10 rounded-full">
                                 <Image src={img} alt='img'></Image>
@@ -34,6 +56,21 @@ const Navbar = () => {
                             <li><a>Settings</a></li>
                             <li><a>Logout</a></li>
                         </ul>
+                        {/* for drawer */}
+                        <div className={`flex ${open ? 'overflow-x-hidden' : ''}`}>
+                        {open && <div className="fixed inset-0 bg-black opacity-50"></div>}
+                                <div onClick={toggleDrawer}>
+                                    <IoMdSettings className='text-green-600 text-xl'></IoMdSettings>
+                                </div>
+                            {/* Drawer */}
+                            {open && (
+                                <div className="fixed top-0 right-0 h-full w-[300px] bg-white text-black rounded-tl-2xl rounded-bl-2xl p-8">
+                                    <h2>Drawer Content</h2>
+                                    
+                                </div>
+                            )}
+                        </div>
+
                     </div>
                 </div>
             </div>
